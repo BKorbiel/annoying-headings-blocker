b6271ac9bc759b3e930dfe90e95db96ee0f3b9cd
Bartlomiej Korbiel <korbiel.bartek@gmail.com>
2023-03-09 19:52:45 +0100

    improved options page

--- options.js
+++ options.js
@@ -1,32 +1,58 @@
-function save() {
+function save() { //saving keywords to chrome storage
     var keywords=[];
-    var elements = document.querySelectorAll('.keyword');
+    var elements = document.querySelectorAll('.keyword-row');
     elements.forEach(element => {
-        keywords.push(element.innerHTML);
+        var keyword = element.querySelector('.keyword').innerHTML;
+        var caseSensitive = element.querySelector('input').checked;
+        keywords.push({keyword: keyword, caseSensitive: caseSensitive});
     });
     chrome.storage.sync.set({
         keywords: keywords
     });
 }
 
-function createKeywordRow(keyword) {
+function createKeywordRow(keyword) { //this func creates new row for keyword in options page
     var newRow = document.createElement('div');
     newRow.className = "keyword-row";
 
     var newKeyword = document.createElement('div');
     newKeyword.className = "keyword";
-    newKeyword.innerHTML = keyword;
+    newKeyword.innerHTML = keyword.keyword; //keyword
+
+    var removeButton = document.createElement('button'); //remove button
+    removeButton.className = "remove";
+    removeButton.innerHTML = "Remove";
+    removeButton.addEventListener('click', function(e){
+        newRow.remove();
+        save();
+    });
+
+    var checkBox = document.createElement('input'); //checkbox for "case sensitive"
+    checkBox.type = "checkbox";
+    checkBox.checked = keyword.caseSensitive;
+    checkBox.addEventListener('change', function(e){
+        save();
+    });
+    var label = document.createElement('label');
+    label.innerHTML = "Case sensitive";
+    var caseSensitive = document.createElement('div');
+    caseSensitive.className = "case-sensitive";
+    caseSensitive.appendChild(checkBox);
+    caseSensitive.appendChild(label);
 
     newRow.appendChild(newKeyword);
+    newRow.appendChild(removeButton);
+    newRow.appendChild(caseSensitive);
 
-    document.querySelector('.keywords-holder').appendChild(newRow);
+    document.querySelector('.keywords-holder').appendChild(newRow); //append new row
+
 }
 
 //adding new keyword
 document.querySelector('.add-keyword').addEventListener('click', function(){
     var keyword = document.getElementById('new-keyword').value;
 
-    createKeywordRow(keyword);
+    createKeywordRow({keyword: keyword, caseSensitive: false});
 
     save();
 
@@ -39,7 +65,6 @@ document.addEventListener('DOMContentLoaded', function(){
     chrome.storage.sync.get({
         keywords: []
     }, function(storage) {
-        console.log(storage);
         storage.keywords.forEach(keyword => {
             createKeywordRow(keyword);
         });
--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 var KEY_WORDS = [];
 
-function getHeadings (element) {
+function getHeadings (element) { //get elements that are possibly headings 
     if (element instanceof Element || element instanceof HTMLDocument) {
         var headings = element.querySelectorAll('h1, h2, h3, h4, h5, a');
         for (var j = 0; j < headings.length; j++) {
             if (headings[j].hasChildNodes()) {
-                headings[j].childNodes.forEach(heading => blockHeading(heading));
+                headings[j].childNodes.forEach(heading => blockHeading(heading)); //we need only elements without any child
             }
         }
     }
 }
 
 function blockHeading (element) {
-    if (element.hasChildNodes()) {
+    if (element.hasChildNodes()) { //we need only elements without any child (text nodes)
         element.childNodes.forEach(child => blockHeading(child));
     } else if (element.nodeType === Text.TEXT_NODE) {
-        for (var i = 0; i < KEY_WORDS.length; i++) {
-            if (element.wholeText.toLowerCase().includes(KEY_WORDS[i].toLowerCase())) {
-                element.parentElement.style.backgroundColor = "black";
-                element.parentElement.style.color = "black";
+        for (var i = 0; i < KEY_WORDS.length; i++) { //check if the text includes any keyword
+            if (KEY_WORDS[i].caseSensitive===true) {
+                if (element.wholeText.includes(KEY_WORDS[i].keyword)) {
+                    element.parentElement.style.backgroundColor = "black";
+                    element.parentElement.style.color = "black";
+                }
+            } else {
+                if (element.wholeText.toLowerCase().includes(KEY_WORDS[i].keyword.toLowerCase())) {
+                    element.parentElement.style.backgroundColor = "black";
+                    element.parentElement.style.color = "black";
+                }
             }
+            break;
         }
     }
 }
@@ -33,6 +41,7 @@ chrome.storage.sync.get({
 });
 
 
+//when new node appers we run the script again to block new headings
 document.addEventListener("DOMNodeInserted", function(e) {
     getHeadings(e.target);
 }, false);
